98c7509ce5a148e3c691b4325e02cf9984d11807
Luis Robles <luisgeremiasrobles@gmail.com>
2026-03-19 12:29:54 -0300

    Posicione los links y los botones en el navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,21 +2,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
 const Menu = () => {
     return (
         <div>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="#home">Pequeños Pasos</Navbar.Brand>
+            <Navbar expand="lg" className="navbar">
+                <Container fluid>
+                    <Navbar.Brand className='logo ms-2'>Pequeños Pasos</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Inicio</Nav.Link>
-                            <Nav.Link href="#link">Tratamientos</Nav.Link>
-                            <Nav.Link href="#link">Tienda</Nav.Link>
-                            <Nav.Link href="#link">Registrarse</Nav.Link>
-                            <Nav.Link href="#link">Solicitar Turno</Nav.Link>
+                        <Nav className="mx-auto align-items-center">
+                            <Nav.Link active>Inicio</Nav.Link>
+                            <Nav.Link>Tratamientos</Nav.Link>
+                            <Nav.Link>Tienda</Nav.Link>
+                        </Nav>
+                        <Nav className='align-items-center me-4'>
+                            <Nav.Link className='me-2'>Registrarse</Nav.Link>
+                            <Button className='btn-principal'>Solicitar Turno</Button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
