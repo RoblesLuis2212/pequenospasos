20da90b8256bf8c2c7ede452980fc77d45903d56
Luis Robles <luisgeremiasrobles@gmail.com>
2026-03-20 10:55:14 -0300

    Adapte el diseño de forma responsive

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,14 +12,14 @@ const Menu = () => {
                     <Navbar.Brand className='logo ms-2'>Pequeños Pasos</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mx-auto align-items-center">
-                            <Nav.Link active>Inicio</Nav.Link>
+                        <Nav className="mx-auto align-items-center mt-1 mt-md-0">
+                            <Nav.Link>Inicio</Nav.Link>
                             <Nav.Link>Tratamientos</Nav.Link>
                             <Nav.Link>Tienda</Nav.Link>
                         </Nav>
                         <Nav className='align-items-center me-4'>
-                            <Nav.Link className='me-2'>Registrarse</Nav.Link>
-                            <Button className='btn-principal'>Solicitar Turno</Button>
+                            <Nav.Link className='ms-4 ms-md-2'>Registrarse</Nav.Link>
+                            <Button className='btn-principal ms-4 ms-md-2'>Iniciar Sesion</Button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
