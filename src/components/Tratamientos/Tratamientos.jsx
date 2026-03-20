78999c768596f31d4276f90d290f72eff32f2b47
Luis Robles <luisgeremiasrobles@gmail.com>
2026-03-20 20:28:21 -0300

    Agregue una seccion de tratamientos y la estilize

--- src/components/Tratamientos/Tratamientos.jsx
+++ src/components/Tratamientos/Tratamientos.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import "./Tratamientos.css"
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Tratamientos = () => {
     return (
-        <section className='container-fluid border border-black mt-5'>
+        <section className='container-fluid mt-5 bg-fondo py-5'>
             <div className="row">
-                <div className="col-12 col-md-6 border border-black">
-                    <h2 className='titulo-tratamientos'>Nuestros Tratamientos</h2>
-                    <p className='text-muted fs-5'>Especialidades diseñadas para transformar dificultades en fortalezas mediante el juego.</p>
+                <div className="col-12 col-lg-6 d-flex d-lg-block flex-column justify-content-center align-items-center">
+                    <h2 className='titulo-tratamientos ms-2'>Nuestros Tratamientos</h2>
+                    <p className='text-muted fs-5 ms-2'>Especialidades diseñadas para transformar dificultades en fortalezas mediante el juego.</p>
                 </div>
-                <div className="col-12 col-md-6 border border-black">
+                <div className="col-12 col-lg-6 py-3 d-flex justify-content-center">
+                    <Link className='link-opacity-100-hover links fs-5'>Ver todas las terapias<i className="bi bi-arrow-bar-right"></i></Link>
                 </div>
             </div>
         </section>
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </BrowserRouter>
 )
