ff04c33382c390f19ab0adc314657fe65c28ff2c
Luis Robles <luisgeremiasrobles@gmail.com>
2026-03-21 21:02:41 -0300

    Agregue un modal que mostrara el detalle del producto que el usuario desee comprar

--- src/components/Productos/CardProducto.jsx
+++ src/components/Productos/CardProducto.jsx
@@ -1,8 +1,17 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import ModalDetalle from './ModalDetalle';
+import { useState } from 'react';
 
 const CardProducto = ({ nombre, imagen, precio }) => {
+    //Estados para abrir la ventana modal
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <Link className='card-link'>
             <Card className='d-flex flex-column mt-4 mt-lg-3'>
@@ -10,10 +19,10 @@ const CardProducto = ({ nombre, imagen, precio }) => {
                 <Card.Body className='d-flex flex-column align-items-start'>
                     <Card.Title className='nombre-producto'>{nombre}</Card.Title>
                     <p className='precio-producto'>${precio}</p>
-                    <Button className='btn-carrito'>Añadir al carrito</Button>
+                    <Button className='btn-carrito' onClick={handleShow}>Ver Más</Button>
                 </Card.Body>
             </Card>
-
+            <ModalDetalle handleClose={handleClose} show={show}></ModalDetalle>
         </Link>
     );
 };
