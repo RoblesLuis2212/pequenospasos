281fe2b5d26b2c47486acb843344ccbd0908e335
Luis Robles <luisgeremiasrobles@gmail.com>
2026-03-24 21:01:14 -0300

    Agregue estilos al nombre de la web en el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <footer className='container-fluid bg-container'>
+        <footer className='container-fluid bg-container py-4'>
             <div className="row">
                 <div className="col-12 col-md-4 col-lg-3 d-flex flex-column align-items-center align-items-md-start order-1">
                     {/* Columna principal */}
-                    <h5 className="logo-footer">Pequeños Pasos</h5>
+                    <h5 className="logo">Pequeños Pasos</h5>
                     <Link className="text-decoration-none link-footer mt-2">Inicio</Link>
                     <Link className="text-decoration-none link-footer mt-2">Nuestros servicios</Link>
                     <Link className="text-decoration-none link-footer mt-2">Preguntas frecuentes</Link>
@@ -16,28 +16,28 @@ const Footer = () => {
                 <div className="col-12 col-md-4 col-lg-3 d-flex flex-column align-items-center align-items-md-start order-2">
                     {/* Columnas con las funcionalidades que manipulará el paciente */}
                     <h5 className="titulo-columnas mt-4 mt-md-0">Portal pacientes</h5>
-                    <Link className="text-decoration-none link-footer">Iniciar Sesion</Link>
+                    <Link className="text-decoration-none link-footer mt-2">Iniciar Sesion</Link>
                     <Link className="text-decoration-none link-footer mt-2">Crear Cuenta</Link>
                     <Link className="text-decoration-none link-footer mt-2">Turnos Online</Link>
                     <Link className="text-decoration-none link-footer mt-2">Ayuda con mi cuenta</Link>
                 </div>
                 <div className="col-12 col-md-4 col-lg-3 d-flex flex-column align-items-center align-items-md-start order-3 order-md-4 order-lg-4">
                     {/* Columna de contacto */}
-                    <h5 className="titulo-columnas mt-4 mt-md-0 mt-md-0">Contacto</h5>
-                    <p className="texto"><i className="bi bi-whatsapp me-2"></i>3863418518</p>
+                    <h5 className="titulo-columnas mt-4 mt-md-0 mt-md-4 mt-lg-0">Contacto</h5>
+                    <p className="texto mt-2"><i className="bi bi-whatsapp me-2"></i>3863418518</p>
                     <p className="texto"><i className="bi bi-envelope-fill me-2 me-md-0 me-lg-2"></i>lucianafono2014@gmail.com</p>
-                    <p className="texto"><i class="bi bi-geo-alt-fill me-2 me-md-0 me-lg-2"></i>Belgrano 625, Monteros Tucumán</p>
+                    <p className="texto"><i className="bi bi-geo-alt-fill me-2 me-md-0 me-lg-2"></i>Belgrano 625, Monteros Tucumán</p>
                     <p className="texto"><i className="bi bi-clock-fill me-2"></i>Lunes, Miercoles y Viernes - 16:30 a 21:00 hs</p>
                 </div>
                 <div className="col-12 col-md-4 col-lg-3 order-4 order-md-3 order-lg-4">
                     {/* Columna de redes sociales */}
 
-                    <h5 className="titulo-columnas d-flex mt-4 mt-md-0">Metodos de pago</h5>
+                    <h5 className="titulo-columnas mt-3 mt-md-0 text-center text-md-start">Metodos de pago</h5>
                     <div className="d-flex flex-column align-items-center align-items-md-start">
-                        <p className="texto"><i className="bi bi-cash-coin me-2"></i>Efectivo</p>
+                        <p className="texto mt-2"><i className="bi bi-cash-coin me-2"></i>Efectivo</p>
                         <p className="texto"><i className="bi bi-bank2 me-2"></i>Transferencia</p>
                         <p className="texto"><i className="bi bi-credit-card-2-back-fill me-2"></i>Tarjeta de debito</p>
-                        <p className="texto"><i class="bi bi-credit-card-2-front-fill me-2"></i>Tarjeta de credito</p>
+                        <p className="texto"><i className="bi bi-credit-card-2-front-fill me-2"></i>Tarjeta de credito</p>
                     </div>
                 </div>
                 <div className="col-12 order-5">
