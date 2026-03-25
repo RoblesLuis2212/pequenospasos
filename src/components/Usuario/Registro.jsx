68a9fbacd37b35f639a9f294d15a3baba1a169d6
Luis Robles <luisgeremiasrobles@gmail.com>
2026-03-25 11:57:00 -0300

    Estilize el formulario y lo adapte para los diferentes dispositivos

--- src/components/Usuario/Registro.jsx
+++ src/components/Usuario/Registro.jsx
@@ -4,11 +4,11 @@ import "./Registro.css";
 
 const Registro = () => {
     return (
-        <section className='container'>
+        <section className='container contenedor-registro form-registro mt-4 mb-3'>
             <h3 className='titulo-registro text-center'>Unete a Pequeños Pasos</h3>
-            <p className='text-muted'>Crea una cuenta para gestionar turnos y el progreso de tu peque</p>
+            <p className='text-muted text-center'>Crea una cuenta para gestionar turnos y el progreso de tu peque</p>
             <div className="row">
-                <div className="col-12 border border-black">
+                <div className="col-12">
                     <FormularioRegistro></FormularioRegistro>
                 </div>
             </div>
