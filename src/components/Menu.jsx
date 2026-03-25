820e816131dfd54b78da08574f7918c6940c929a
Luis Robles <luisgeremiasrobles@gmail.com>
2026-03-25 12:04:18 -0300

    Agregue una componente con la ventana modal para iniciar sesion

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,16 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
+import ModalIniciarSesion from './Usuario/ModalIniciarSesion';
+import { useState } from 'react';
 
 const Menu = () => {
+    // Estados para abrir el modal de inicio de sesion
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div>
             <Navbar expand="lg" className="navbar">
@@ -19,11 +27,12 @@ const Menu = () => {
                         </Nav>
                         <Nav className='align-items-center me-4'>
                             <Nav.Link className='ms-4 ms-md-2'>Registrarse</Nav.Link>
-                            <Button className='btn-principal ms-4 ms-md-2'>Iniciar Sesion</Button>
+                            <Button className='btn-principal ms-4 ms-md-2' onClick={handleShow}>Iniciar Sesion</Button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <ModalIniciarSesion handleClose={handleClose} show={show}></ModalIniciarSesion>
         </div>
     );
 };
