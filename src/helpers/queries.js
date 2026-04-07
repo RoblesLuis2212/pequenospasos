f7be219d5f2349313c9a3fb405128ec8fa16c408
Luis Robles <luisgeremiasrobles@gmail.com>
2026-04-07 14:35:39 -0300

    Agregue la conexion con el back para poder registrar un paciente

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,4 +1,5 @@
 const usuariosBackend = import.meta.env.VITE_API_USUARIOS;
+const pacientesBackend = import.meta.env.VITE_API_PACIENTES;
 
 export const login = async (usuario) => {
   try {
@@ -24,6 +25,23 @@ export const registro = async (usuario) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(usuario),
+    });
+    return respuesta;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const registroPacientes = async (paciente) => {
+  try {
+    const respuesta = await fetch(pacientesBackend, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-token": JSON.parse(sessionStorage.getItem("usuarioKey")).token,
+      },
+      body: JSON.stringify(paciente),
     });
     return respuesta;
   } catch (err) {
