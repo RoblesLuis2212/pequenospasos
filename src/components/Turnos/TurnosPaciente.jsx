1dcb2409b8aba4c5f8cb0269fc0cd131f5c7819f
Luis Robles <luisgeremiasrobles@gmail.com>
2026-04-10 19:11:07 -0300

    Agregue un subtitulo a la web

--- src/components/Turnos/TurnosPaciente.jsx
+++ src/components/Turnos/TurnosPaciente.jsx
@@ -4,9 +4,10 @@ import TablaTurnos from './TablaTurnos';
 const TurnosPaciente = () => {
     return (
         <section className='container mb-5'>
-            <h3 className='text-center titulo mt-3 mb-2'>Gestioná y controlá el turno de tus niños</h3>
+            <h3 className='text-center titulo mt-3 mb-2'>Turnos pacientes</h3>
+            <p className='text-muted text-center'>Gestioná y controlá el turno de tus niños</p>
             <div className="row">
-                <div className="col-12 tabla-container mt-2">
+                <div className="col-12 tabla-container mt-">
                     <TablaTurnos></TablaTurnos>
                 </div>
             </div>
