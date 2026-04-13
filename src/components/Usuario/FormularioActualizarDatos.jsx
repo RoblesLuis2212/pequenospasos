e54dc8a5c6ba215904a6562193effeedfc40bfa9
Luis Robles <luisgeremiasrobles@gmail.com>
2026-04-13 20:23:42 -0300

    valide y estilize el formulario para actualizar datos

--- src/components/Usuario/FormularioActualizarDatos.jsx
+++ src/components/Usuario/FormularioActualizarDatos.jsx
@@ -1,30 +1,87 @@
 import Button from 'react-bootstrap/Button';
+import InputGroupText from 'react-bootstrap/esm/InputGroupText';
 import Form from 'react-bootstrap/Form';
+import { useForm } from 'react-hook-form';
+import { InputGroup } from 'react-bootstrap';
 
 const FormularioActualizarDatos = () => {
+    const { handleSubmit, register, formState: { errors }, reset, clearErrors } = useForm();
+
+    const postValidaciones = (data) => {
+        console.log(data);
+    }
+
     return (
-        <Form>
-            <Form.Group>
+        <Form onSubmit={handleSubmit(postValidaciones)}>
+            <Form.Group className='mb-3'>
                 <Form.Label className='etiquetas'>Nombre completo</Form.Label>
-                <Form.Control className='input-pildora' type="text" placeholder="Ej: Juan Perez" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
+                <InputGroup className='input-pildora'>
+                    <InputGroupText>
+                        <i className="bi bi-house-door-fill"></i>
+                    </InputGroupText>
+                    <Form.Control type="text" placeholder="Ej: Juan Perez"
+                        {...register("nombreCompleto", {
+                            required: "Este campo es obligatorio",
+                            minLength: {
+                                value: 12,
+                                message: "El nombre debe contener minimo 12 caracteres"
+                            },
+                            maxLength: {
+                                value: 100,
+                                message: "El nombre debe contener minimo 100 caracteres"
+                            }
+                        })}
+                        onChange={() => clearErrors("nombreCompleto")}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.nombreCompleto?.message}
+                    </Form.Text>
+                </InputGroup>
             </Form.Group>
 
             <Form.Group className="mb-3">
                 <Form.Label className='etiquetas'>Telefono</Form.Label>
-                <Form.Control className='input-pildora' type="text" placeholder="ej: España 120" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
+                <InputGroup className='input-pildora'>
+                    <InputGroupText>
+                        <i className="bi bi-telephone-fill"></i>
+                    </InputGroupText>
+                    <Form.Control type="text" placeholder="ej: España 120"
+                        {...register("telefono", {
+                            required: "Este dato es obligatorio",
+                            minLength: {
+                                value: 10,
+                                message: "El telefono debe contener minimo 10 caracteres"
+                            },
+                            maxLength: {
+                                value: 15,
+                                message: "El telefono debe contener minimo 15 digitos"
+                            }
+                        })}
+                        onChange={() => clearErrors("telefono")}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.telefono?.message}
+                    </Form.Text>
+                </InputGroup>
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label className='etiquetas'>Email</Form.Label>
-                <Form.Control className='input-pildora' type="email" placeholder="ej: juanperez@gmail.com" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
+                <InputGroup className='input-pildora'>
+                    <InputGroupText>
+                        <i className="bi bi-envelope-at-fill"></i>
+                    </InputGroupText>
+                    <Form.Control type="email" placeholder="ej: juanperez@gmail.com"
+                        {...register("email", {
+                            required: "Este campo es obligatorio",
+                            pattern: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                            message: "Email no valido"
+                        })}
+                        onChange={() => clearErrors("email")}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.email?.message}
+                    </Form.Text>
+                </InputGroup>
             </Form.Group>
             <div className="d-flex flex-column justify-content-center">
                 <Button className='btn-principal mt-2' type="submit">
--- src/components/Usuario/ModalActualizarDatos.jsx
+++ src/components/Usuario/ModalActualizarDatos.jsx
@@ -7,7 +7,7 @@ const ModalActualizarDatos = ({ showPadres, cerrarModalPadre }) => {
         <Modal show={showPadres} onHide={cerrarModalPadre}>
             <div className='d-flex flex-column p-3'>
                 <h4 className='text-center titulo mt-2'>Mis datos personales</h4>
-                <p className='text-center text-muted m-0'>Mantené tus datos personales actualizados para no perderte ninguna novedad.</p>
+                <p className='text-center text-muted'>Mantené tus datos personales actualizados para no perderte ninguna novedad.</p>
             </div>
             <Modal.Body>
                 <FormularioActualizarDatos></FormularioActualizarDatos>
