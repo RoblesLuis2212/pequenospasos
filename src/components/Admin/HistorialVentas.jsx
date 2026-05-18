2f23cf63520d190f5d05263d310f19313a120259
Luis Robles <luisgeremiasrobles@gmail.com>
2026-05-18 16:21:46 -0300

    agregue un componente historial de ventas y una ruta para acceder a el

---
+++ src/components/Admin/HistorialVentas.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HistorialVentas = () => {
+    return (
+        <section className='container'>
+            <div className="row">
+                <div className="col-12 col-md-6 border border-black">1234</div>
+                <div className="col-12 col-md-6 border border-black">1234</div>
+                <div className="col-12 col-md-6 border border-black">1234</div>
+                <div className="col-12 col-md-6 border border-black">1234</div>
+            </div>
+        </section>
+    );
+};
+
+export default HistorialVentas;
