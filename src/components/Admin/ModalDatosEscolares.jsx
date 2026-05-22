9c8c110d1996aa55b0d9c180b6b6e5c9d0673b30
Luis Robles <luisgeremiasrobles@gmail.com>
2026-05-22 11:11:27 -0300

    valide el formulario de datos escolare

--- src/components/Admin/ModalDatosEscolares.jsx
+++ src/components/Admin/ModalDatosEscolares.jsx
@@ -1,32 +1,59 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 const ModalDatosEscolares = ({ cerrarModalEscolar, showModalEscolar }) => {
+
+    const { register, handleSubmit, formState: { errors }, clearErrors, reset } = useForm();
+
+    const postValidaciones = (data) => {
+        console.log(data);
+    }
+
+
     return (
         <Modal show={showModalEscolar} onHide={cerrarModalEscolar}>
             <Modal.Header className='d-flex justify-content-center'>
                 <h4 className='titulo'>Datos escolares</h4>
             </Modal.Header>
             <Modal.Body>
-                <Form>
+                <Form onSubmit={handleSubmit(postValidaciones)}>
                     <Form.Group className="mb-3">
                         <Form.Label className='etiquetas'>Escuela</Form.Label>
-                        <Form.Control type="text" placeholder="ej: Escuela Jose Federico Moreno" className='custom-input input-form' />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
+                        <Form.Control type="text" placeholder="ej: Escuela Jose Federico Moreno" className='custom-input input-form'
+                            {...register("escuela", {
+                                required: "La escuela a la que asiste el paciente es un dato obligatorio",
+                                minLength: {
+                                    value: 10,
+                                    message: "El campo debe contener minimo 10 caracteres"
+                                },
+                                maxLength: {
+                                    value: 60,
+                                    message: "El campo debe contener minimo 60 caracteres"
+                                }
+                            })}
+                            onChange={() => clearErrors("escuela")}
+                        />
+                        <Form.Text className="text-danger">
+                            {errors.escuela?.message}
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label className='etiquetas'>Escuela</Form.Label>
-                        <Form.Select className='custom-input input-form'>
+                        <Form.Select className='custom-input input-form'
+                            {...register("turno", {
+                                required: "Por favor seleccione turno"
+                            })}
+                            onChange={() => clearErrors("turno")}
+                        >
                             <option value="">Seleccione un turno</option>
                             <option value="MAÑANA">Mañana</option>
                             <option value="TARDE">Tarde</option>
                             <option value="NOCHE">Noche</option>
                         </Form.Select>
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
+                        <Form.Text className="text-danger">
+                            {errors.turno?.message}
                         </Form.Text>
                     </Form.Group>
                     <Button className='btn-principal w-100' type="submit">
